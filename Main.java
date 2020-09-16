1cd7a936878013ba68ff73a1cead9aac6625c879
blisn8405 <blisn8405@wrdsb.ca>
2020-09-16 13:22:35 +0000

    finished summative

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
  *
- * @author 
+ * @nick 
  */
 public class Main {
 
@@ -9,7 +11,48 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+  Scanner input = new Scanner(System.in);
+
+ // ask the user for the pizza size in cm
+ System.out.println("What is the size of the pizza in cm:");
+  
+ // declare and initialize a variable for the pizza
+double diameter = input.nextDouble();
+
+// labour for pizza
+final double labour = .75;
+
+// power for oven
+final double power = .99;
+
+
+// taxes of pizza
+final double tax = diameter * .13;
+
+// price for ingredients
+final double ingredients = diameter * .5;
+
+if(diameter >= 1 && diameter <= 20){
+double subtotal = (diameter * .5 + power + labour);
+System.out.println("Subtotal " + subtotal);
+System.out.println("Taxes " + tax);
+System.out.println("Total " + subtotal + tax);
+System.out.println("We are going to make you a cute little pizza!");
+} else 
+if(diameter >= 20 && diameter <= 40){
+double subtotal = (diameter * .5 + power + labour);
+System.out.println("Subtotal " + subtotal);
+System.out.println("Taxes " + tax);
+System.out.println("Total " + subtotal + tax);
+System.out.println("This will be delicious!");
+} else 
+if(diameter > 40){
+double subtotal = (diameter * .5 + power + labour);
+System.out.println("Subtotal " + subtotal);
+System.out.println("Taxes " + tax);
+System.out.println("Total " + subtotal + tax);
+System.out.println("Whoa, big pizza! You might need a truck to get this home!");
+}  
   }
 }
